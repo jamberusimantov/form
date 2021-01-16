e3de02db6912af15ad44b050f7eaa897f7358095
jamberusimantov <jamberu.simantov@gmail.com>
2021-01-16 12:32:18 +0200

    add reset btn

--- index.ts
+++ index.ts
@@ -7,10 +7,11 @@ inputAttArr.push(['telInput', 'PHONE NUMBER', 'tel']);
 inputAttArr.push(['usernameInput', 'USERNAME', 'text']);
 inputAttArr.push(['passwordInput', 'PASSWORD', 'password']);
 inputAttArr.push(['signUpBtn', 'sign Up', 'submit']);
+inputAttArr.push(['resetBtn', 'reset', 'reset']);
 document.getElementById('body').innerHTML += "<form id='form' onsubmit='return validate()'></form>";
 inputAttArr.forEach(printInput);
 function printInput(arr: string[]): void {
-    document.getElementById('form').innerHTML += (arr[0] == 'signUpBtn') ? `<button id='${arr[0]}' type="${arr[2]}">${arr[1]}</button>` : `<label class='label' for='${arr[0]}'>${arr[1]}:<span class='alert' id='${arr[0]}Alert'></span></label><input class='input' name='${arr[0]}' id='${arr[0]}' type='${arr[2]}'>`;
+    document.getElementById('form').innerHTML += (arr[0] == 'signUpBtn' || arr[0] == 'resetBtn') ? `<button id='${arr[0]}' type="${arr[2]}">${arr[1]}</button>` : `<label class='label' for='${arr[0]}'>${arr[1]}:<span class='alert' id='${arr[0]}Alert'></span></label><input class='input' name='${arr[0]}' id='${arr[0]}' type='${arr[2]}'>`;
 }
 function validate(): boolean {
     let fName: string = (<HTMLInputElement>document.getElementById(inputAttArr[0][0])).value.trim();
