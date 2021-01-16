2fe013482425582e52584b7574f4496895e94c5a
jamberusimantov <jamberu.simantov@gmail.com>
2021-01-16 16:14:58 +0200

    add password check

--- index.js
+++ index.js
@@ -6,14 +6,18 @@ inputAttArr.push(['imgInput', 'PROFILE PIC', 'file']);
 inputAttArr.push(['telInput', 'PHONE NUMBER', 'tel']);
 inputAttArr.push(['usernameInput', 'USERNAME', 'text']);
 inputAttArr.push(['passwordInput', 'PASSWORD', 'password']);
+inputAttArr.push(['passwordCheckInput', 'PASSWORD VALIDATION', 'password']);
 inputAttArr.push(['signUpBtn', 'sign Up', 'submit']);
 inputAttArr.push(['resetBtn', 'reset', 'reset']);
-document.getElementById('body').innerHTML += "<form id='form' onsubmit='return validate()'></form>";
+document.getElementById('body').innerHTML += "<form id='form' onsubmit='return validateForm()'></form>";
 inputAttArr.forEach(printInput);
 function printInput(arr) {
     document.getElementById('form').innerHTML += (arr[0] == 'signUpBtn' || arr[0] == 'resetBtn') ? "<button id='" + arr[0] + "' type=\"" + arr[2] + "\">" + arr[1] + "</button>" : "<label class='label' for='" + arr[0] + "'>" + arr[1] + ":<span class='alert' id='" + arr[0] + "Alert'></span></label><input class='input' name='" + arr[0] + "' id='" + arr[0] + "' type='" + arr[2] + "'>";
 }
-function validate() {
+function alertInput(index, text) {
+    document.getElementById(inputAttArr[index][0] + "Alert").innerText = text;
+}
+function validateForm() {
     var fName = document.getElementById(inputAttArr[0][0]).value.trim();
     var lName = document.getElementById(inputAttArr[1][0]).value.trim();
     var email = document.getElementById(inputAttArr[2][0]).value.trim();
@@ -21,14 +25,22 @@ function validate() {
     var phone = document.getElementById(inputAttArr[4][0]).value.trim();
     var username = document.getElementById(inputAttArr[5][0]).value.trim();
     var password = document.getElementById(inputAttArr[6][0]).value.trim();
-    var values = [fName, lName, email, photo, phone, username, password];
-    var fail = values.indexOf('');
-    if (fail != -1) {
-        for (var i = 0; i < fail; i++) {
-            document.getElementById(inputAttArr[i][0] + "Alert").innerText = '';
+    var passwordCheck = document.getElementById(inputAttArr[7][0]).value.trim();
+    var mandatoryValues = [fName, lName, email, photo, phone, username, password, passwordCheck];
+    var blank = mandatoryValues.indexOf('');
+    var passwordArr = mandatoryValues.splice(6);
+    if (blank != -1) {
+        for (var i = 0; i < blank; i++) {
+            alertInput(i, '');
         }
-        document.getElementById(inputAttArr[fail][0] + "Alert").innerText = '*required';
+        alertInput(blank, '*required');
         return false;
     }
-    return true;
+    else {
+        if (passwordArr[0] != passwordArr[1]) {
+            alertInput(7, '*no match');
+            return false;
+        }
+        return true;
+    }
 }
--- index.ts
+++ index.ts
@@ -6,14 +6,18 @@ inputAttArr.push(['imgInput', 'PROFILE PIC', 'file']);
 inputAttArr.push(['telInput', 'PHONE NUMBER', 'tel']);
 inputAttArr.push(['usernameInput', 'USERNAME', 'text']);
 inputAttArr.push(['passwordInput', 'PASSWORD', 'password']);
+inputAttArr.push(['passwordCheckInput', 'PASSWORD VALIDATION', 'password']);
 inputAttArr.push(['signUpBtn', 'sign Up', 'submit']);
 inputAttArr.push(['resetBtn', 'reset', 'reset']);
-document.getElementById('body').innerHTML += "<form id='form' onsubmit='return validate()'></form>";
+document.getElementById('body').innerHTML += "<form id='form' onsubmit='return validateForm()'></form>";
 inputAttArr.forEach(printInput);
 function printInput(arr: string[]): void {
     document.getElementById('form').innerHTML += (arr[0] == 'signUpBtn' || arr[0] == 'resetBtn') ? `<button id='${arr[0]}' type="${arr[2]}">${arr[1]}</button>` : `<label class='label' for='${arr[0]}'>${arr[1]}:<span class='alert' id='${arr[0]}Alert'></span></label><input class='input' name='${arr[0]}' id='${arr[0]}' type='${arr[2]}'>`;
 }
-function validate(): boolean {
+function alertInput(index: number, text: string): void {
+    document.getElementById(`${inputAttArr[index][0]}Alert`).innerText = text;
+}
+function validateForm(): boolean {
     let fName: string = (<HTMLInputElement>document.getElementById(inputAttArr[0][0])).value.trim();
     let lName: string = (<HTMLInputElement>document.getElementById(inputAttArr[1][0])).value.trim();
     let email: string = (<HTMLInputElement>document.getElementById(inputAttArr[2][0])).value.trim();
@@ -21,14 +25,22 @@ function validate(): boolean {
     let phone: string = (<HTMLInputElement>document.getElementById(inputAttArr[4][0])).value.trim();
     let username: string = (<HTMLInputElement>document.getElementById(inputAttArr[5][0])).value.trim();
     let password: string = (<HTMLInputElement>document.getElementById(inputAttArr[6][0])).value.trim();
-    let values: string[] = [fName, lName, email, photo, phone, username, password];
-    let fail: number = values.indexOf('');
-    if (fail != -1) {
-        for (let i = 0; i < fail; i++) {
-            document.getElementById(`${inputAttArr[i][0]}Alert`).innerText = '';
+    let passwordCheck: string = (<HTMLInputElement>document.getElementById(inputAttArr[7][0])).value.trim();
+    let mandatoryValues: string[] = [fName, lName, email, photo, phone, username, password, passwordCheck];
+    let blank: number = mandatoryValues.indexOf('');
+    let passwordArr: string[] = mandatoryValues.splice(6);
+    if (blank != -1) {
+        for (let i = 0; i < blank; i++) {
+            alertInput(i, '');
         }
-        document.getElementById(`${inputAttArr[fail][0]}Alert`).innerText = '*required'
+        alertInput(blank, '*required');
         return false;
     }
-    return true;
+    else {
+        if (passwordArr[0] != passwordArr[1]) {
+            alertInput(7, '*no match');
+            return false;
+        }
+        return true;
+    }
 }
